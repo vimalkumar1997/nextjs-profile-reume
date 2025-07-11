35d52be136e8fb0517867fa4a3968970ef67be05
unknown <vimalkumarrenu@gmail.com>
2025-07-11 06:04:12 +0530

    next js deployment

--- src/pages/personal_profile/index.tsx
+++ src/pages/personal_profile/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <AboutusComponent/>
         <ResumeComponent/>
         <MySkillsComponent/>
-        <ProjectsComponent/>
+        {/* <ProjectsComponent/> */}
       </div>
     </>
   );
